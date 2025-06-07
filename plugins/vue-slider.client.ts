8ee680097bb761a30d7f9e32b21375fb8ae36dc4
Nicolas GUEVARA <nicoguev333@gmail.com>
2025-06-07 18:57:45 +0200

    Remove vue slider (now use USlider)

--- plugins/vue-slider.client.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import VueSlider from 'vue-slider-component/dist-css/vue-slider-component.umd.min.js'
-import 'vue-slider-component/dist-css/vue-slider-component.css'
-import 'vue-slider-component/theme/default.css'
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(VueSlider)
-})
